6ff4cf926324d05093dd485310ae73b46fd88865
CryptoCOB <38410716+CryptoCOB@users.noreply.github.com>
2025-06-12 03:22:30 -0400

    Fix handlers package and deduplicate code

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,11 @@
 """Integration handlers package."""
+from .arc_llm_handler import ARCLLMHandler
 from .rag_integration_handler import RagIntegrationHandler
 from .speech_integration_handler import SpeechIntegrationHandler
 from .vmb_integration_handler import VMBIntegrationHandler
 
 __all__ = [
+    "ARCLLMHandler",
     "RagIntegrationHandler",
     "SpeechIntegrationHandler",
     "VMBIntegrationHandler",
